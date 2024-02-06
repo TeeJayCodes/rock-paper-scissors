9931c5aa5d8e41a330b3a86b6fd28c40275f9928
TJ <TimothyJCar@gmail.com>
2024-02-06 18:37:10 -0500

    Add function to make a selection case-insensitive
    This will allow one of the three valid strings to be accepted regardless of format

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ function getRandomNumber() {
     choice = Math.ceil(Math.random() * 3);
     return choice;
 }
+
+//Write a function that plays one round of Rock Paper Scissors. It should take two parameters, one being case-insensitive playerSelection and the other being computerSelection. Then return a string that declares the winner or a tie
+
+//formatSelection make a selection case-insensitive so the returned string will always only have the first letter capitalized
+function formatSelection(selection) {
+    let firstLetter = selection.charAt(0);
+    let firstLetterUppercase = firstLetter.toUpperCase();
+    let remainingLetters = selection.substring(1);
+    let remainingLettersLowercase = remainingLetters.toLowerCase();
+    let selectionFinalFormat = firstLetterUppercase + remainingLettersLowercase;
+    return selectionFinalFormat;
+}
