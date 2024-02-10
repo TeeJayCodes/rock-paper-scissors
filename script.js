c8a96a8116cc426383b9a1956addbd31df497907
TJ <TimothyJCar@gmail.com>
2024-02-10 13:19:43 -0500

    Add playGame function
    This keeps track of the score for 5 rounds and declares the winner

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//Create a function named getComputerChoice that randomly returns 'Rock', 'Paper', or 'Scissors'. getComputerChoice calls the getRandomNumber function, and uses its return value to decide which of the 3 choices to return
 let choice;
 let playerSelection;
 let computerSelection;
 let message;
+let gameWinner;
+let playerWinCount = 0;
+let computerWinCount = 0;
 
+
+//getComputerChoice randomly selects 'Rock', 'Paper', or 'Scissors' for the computer
 function getComputerChoice() {
     getRandomNumber();
     switch(choice) {
@@ -26,12 +30,36 @@ function getRandomNumber() {
     return choice;
 }
 
-//Write a function that plays one round of Rock Paper Scissors. It should take two parameters, one being case-insensitive playerSelection and the other being computerSelection. Then return a string that declares the winner or a tie
-function playGame(playerSelection, computerSelection) {
+//playRound takes two parameters, one being case-insensitive playerSelection and the other being computerSelection. It returns a string that declares the round's winner
+function playRound(playerSelection, computerSelection) {
     playerSelection = formatSelection(playerSelection); 
     computerSelection = getComputerChoice();
     pickWinner(playerSelection, computerSelection);
     console.log(message);
+    return message;
+}
+
+//playGame keeps score for five rounds and announces the game's winner at the end
+function playGame(playerSelection) {
+    for (let i = 0; i < 5; ++i) {
+        playerSelection = prompt("Rock, Paper, or Scissors?");
+        playRound(playerSelection);
+        if (message.includes("Win")) {
+            playerWinCount = ++playerWinCount;
+        } else if (message.includes("Lose")) {
+            computerWinCount = ++computerWinCount;
+        }
+    }
+    if (playerWinCount > computerWinCount) {
+        gameWinner = `You Win the game! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
+    } else if (playerWinCount < computerWinCount) {
+        gameWinner = `You Lose the game! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
+    } else {
+        gameWinner = `The game is a tie! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
+    }
+    playerWinCount = 0;
+    computerWinCount = 0;
+    return gameWinner;
 }
 
 //formatSelection makes a selection case-insensitive so the returned string will always only have the first letter capitalized
@@ -48,25 +76,25 @@ function formatSelection(selection) {
 function pickWinner(playerSelection, computerSelection) {
     switch(true) {
         case playerSelection === computerSelection:
-            message = "It's a tie!";
+            message = "It's a tie! Nobody wins this round.";
             break;
         case playerSelection === "Rock" && computerSelection === "Paper":
-            message = "You Lose! Paper beats Rock";
+            message = "You Lose this round! Paper beats Rock";
             break;
         case playerSelection === "Rock" && computerSelection === "Scissors":
-            message = "You Win! Rock beats Scissors";
+            message = "You Win this round! Rock beats Scissors";
             break;
         case playerSelection === "Scissors" && computerSelection === "Rock":
-            message = "You Lose! Rock beats Scissors";
+            message = "You Lose this round! Rock beats Scissors";
             break;
         case playerSelection === "Scissors" && computerSelection === "Paper":
-            message = "You Win! Scissors beats Paper";
+            message = "You Win this round! Scissors beats Paper";
             break;
         case playerSelection === "Paper" && computerSelection === "Rock":
-            message = "You Win! Paper beats Rock";
+            message = "You Win this round! Paper beats Rock";
             break;
         case playerSelection === "Paper" && computerSelection === "Scissors":
-            message = "You Lose! Scissors beats Paper";
+            message = "You Lose this round! Scissors beats Paper";
             break;
     }
     return message;
