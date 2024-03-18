2a85c4b39710c8259325a986e83fd8f37e006147
TJ <TimothyJCar@gmail.com>
2024-03-18 11:14:12 -0400

    Add user interface
    Allows users to play without needing the console for input or output

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let message;
 let gameWinner;
 let playerWinCount = 0;
 let computerWinCount = 0;
+let roundResult = document.createElement(`div`);
 
 
 //getComputerChoice randomly selects 'Rock', 'Paper', or 'Scissors' for the computer
@@ -34,32 +35,14 @@ function getRandomNumber() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = formatSelection(playerSelection); 
     computerSelection = getComputerChoice();
-    pickWinner(playerSelection, computerSelection);
-    console.log(message);
+    pickRoundWinner(playerSelection, computerSelection);
+    roundResult.textContent = message;
+    if (message.includes("Win")) {
+        playerWinCount = ++playerWinCount;
+    } else if (message.includes("Lose")) {
+        computerWinCount = ++computerWinCount;
+    }
     return message;
-}
-
-//playGame keeps score for five rounds and announces the game's winner at the end
-function playGame(playerSelection) {
-    for (let i = 0; i < 5; ++i) {
-        playerSelection = prompt("Rock, Paper, or Scissors?");
-        playRound(playerSelection);
-        if (message.includes("Win")) {
-            playerWinCount = ++playerWinCount;
-        } else if (message.includes("Lose")) {
-            computerWinCount = ++computerWinCount;
-        }
-    }
-    if (playerWinCount > computerWinCount) {
-        gameWinner = `You Win the game! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
-    } else if (playerWinCount < computerWinCount) {
-        gameWinner = `You Lose the game! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
-    } else {
-        gameWinner = `The game is a tie! Your score is ${playerWinCount} and the computer's score is ${computerWinCount}.`;
-    }
-    playerWinCount = 0;
-    computerWinCount = 0;
-    return gameWinner;
 }
 
 //formatSelection makes a selection case-insensitive so the returned string will always only have the first letter capitalized
@@ -72,30 +55,73 @@ function formatSelection(selection) {
     return selectionFinalFormat;
 }
 
-//pickWinner determines who the winner is, or if it's a tie, and returns the appropriate message
-function pickWinner(playerSelection, computerSelection) {
+//pickRoundWinner determines who the round winner is, or if it's a tie, and returns the appropriate message
+function pickRoundWinner(playerSelection, computerSelection) {
     switch(true) {
         case playerSelection === computerSelection:
             message = "It's a tie! Nobody wins this round.";
             break;
         case playerSelection === "Rock" && computerSelection === "Paper":
-            message = "You Lose this round! Paper beats Rock";
+            message = "You Lose this round! Paper beats Rock!";
             break;
         case playerSelection === "Rock" && computerSelection === "Scissors":
-            message = "You Win this round! Rock beats Scissors";
+            message = "You Win this round! Rock beats Scissors!";
             break;
         case playerSelection === "Scissors" && computerSelection === "Rock":
-            message = "You Lose this round! Rock beats Scissors";
+            message = "You Lose this round! Rock beats Scissors!";
             break;
         case playerSelection === "Scissors" && computerSelection === "Paper":
-            message = "You Win this round! Scissors beats Paper";
+            message = "You Win this round! Scissors beats Paper!";
             break;
         case playerSelection === "Paper" && computerSelection === "Rock":
-            message = "You Win this round! Paper beats Rock";
+            message = "You Win this round! Paper beats Rock!";
             break;
         case playerSelection === "Paper" && computerSelection === "Scissors":
-            message = "You Lose this round! Scissors beats Paper";
+            message = "You Lose this round! Scissors beats Paper!";
             break;
     }
     return message;
 }
+
+//pickGameWinner prints the current score and, if there is one, the game winner, then resets the score
+function pickGameWinner() {
+    if (playerWinCount === 5) {
+        PLAYERWINS.textContent = ` You win the game! The final score is Player - ${playerWinCount} and Computer - ${computerWinCount}`;
+        roundResult.append(PLAYERWINS);
+        playerWinCount = 0;
+        computerWinCount = 0;
+    } else if (computerWinCount === 5) {
+        COMPUTERWINS.textContent = ` You lose the game! The final score is Player - ${playerWinCount} and Computer - ${computerWinCount}`;
+        roundResult.append(COMPUTERWINS);
+        playerWinCount = 0;
+        computerWinCount = 0;
+    } else {
+    document.body.append(roundResult);
+    roundResult.append(SCORE);
+    }
+}
+
+
+const ROCK = document.querySelector(`.rock`)
+const PAPER = document.querySelector(`.paper`)
+const SCISSORS = document.querySelector(`.scissors`)
+const SCORE = document.createElement(`h3`);
+const PLAYERWINS = document.createElement(`h2`);
+const COMPUTERWINS = document.createElement(`h2`);
+
+
+ROCK.addEventListener(`click`, function() {
+    playRound(`Rock`);
+    SCORE.textContent = ` Player score is ${playerWinCount} - Computer Score is ${computerWinCount}`;
+    pickGameWinner();
+});
+PAPER.addEventListener(`click`, function() {
+    playRound(`Paper`);
+    SCORE.textContent = ` Player score is ${playerWinCount} - Computer Score is ${computerWinCount}`;
+    pickGameWinner();
+});
+SCISSORS.addEventListener(`click`, function() {
+    playRound(`Scissors`);
+    SCORE.textContent = ` Player score is ${playerWinCount} - Computer Score is ${computerWinCount}`;
+    pickGameWinner();
+});
